3ae9d2bdf44be270ddb113dee125cd363568080e
EtienneDel <etienne.delnott@viacesi.fr>
2019-04-24 11:41:24 +0200

    landing page features part animated (just need to add docker logo)

--- src/app/components/land/land.component.ts
+++ src/app/components/land/land.component.ts
@@ -1,9 +1,30 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from "@angular/animations";
 
 @Component({
   selector: "app-land",
   templateUrl: "./land.component.html",
-  styleUrls: ["./land.component.scss"]
+  styleUrls: ["./land.component.scss"],
+  animations: [
+    trigger("animStateXLR", [
+      state("in", style({ transform: "translateX(0)", opacity: 1 })),
+      state("void", style({ transform: "translateX(-20px)", opacity: 0 })),
+      transition("void => *", animate(".3s ease-in-out")),
+      transition("* => void", animate(".3s ease-in-out"))
+    ]),
+    trigger("animStateXRL", [
+      state("in", style({ transform: "translateX(0)", opacity: 1 })),
+      state("void", style({ transform: "translateX(20px)", opacity: 0 })),
+      transition("void => *", animate(".3s ease-in-out")),
+      transition("* => void", animate(".3s ease-in-out"))
+    ])
+  ]
 })
 export class LandComponent {
   displayLeft: boolean = false;
